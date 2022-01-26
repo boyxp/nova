aa17c64ea345e3ce6368f2c459f4613d72f8e31e
lee <isme@live.com>
2022-01-26 11:14:55 +0800

    增加私有方法

--- controller/User.go
+++ controller/User.go
@@ -9,9 +9,13 @@ func init() {
 
 type User struct {}
 func (C *User) Login(name string, age int) {
-	fmt.Println(name, age, "ok")
+	fmt.Println(name, age, "hello")
 }
 
 func (C *User) Logout() {
 	fmt.Println("bye")
+}
+
+func (C *User) private() {
+	fmt.Println("private")
 }
