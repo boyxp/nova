380a374cb470c5d3b9a6beb037b8a86db7d9a026
lee <isme@live.com>
2022-01-26 15:51:35 +0800

    返回值

--- controller/User.go
+++ controller/User.go
@@ -2,18 +2,20 @@ package controller
 
 import "fmt"
 import "nova/router"
+import "time"
 
 func init() {
    router.Register(&User{})
 }
 
 type User struct {}
-func (C *User) Login(name string, age int) {
-	fmt.Println(name, age, "hello")
+func (C *User) Login(name string, age int) string {
+	time.Sleep(time.Duration(10 * time.Second))
+	return name;
 }
 
-func (C *User) Logout() {
-	fmt.Println("bye")
+func (C *User) Logout() string {
+	return "bye"
 }
 
 func (C *User) private() {
