121a2441d90fed71761c7ab93d5b0466fd8fdfee
lee <isme@live.com>
2022-01-26 19:22:31 +0800

    增加类型

--- _demo/controller/User.go
+++ _demo/controller/User.go
@@ -2,15 +2,14 @@ package controller
 
 import "fmt"
 import "github.com/boyxp/nova/router"
-import "time"
 
 func init() {
    router.Register(&User{})
 }
 
 type User struct {}
-func (C *User) Login(name string, age int) string {
-	time.Sleep(time.Duration(10 * time.Second))
+func (C *User) Login(name string, age int, check bool, balance float64) string {
+	fmt.Println(name, age, check, balance)
 	return name;
 }
 
