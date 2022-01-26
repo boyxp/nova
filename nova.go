bcb4b9791c4a2241a43f4098a7e74a8f16a5d6ce
lee <isme@live.com>
2022-01-26 16:49:59 +0800

    包名

--- nova.go
+++ nova.go
@@ -1,10 +1,1 @@
-package main
-
-import "nova/app"
-import "nova/request"
-import "nova/response"
-import _ "nova/controller"
-
-func main() {
-	app.New(":8080").SetRequest(&request.Form{}).SetResponse(&response.Json{}).Run()
-}
+package nova
