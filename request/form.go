360391a9f4785f105020607863075d587749870e
lee <isme@live.com>
2022-01-27 15:51:17 +0800

    动态读取POST参数

--- request/form.go
+++ request/form.go
@@ -5,5 +5,13 @@ import "net/http"
 type Form struct {}
 
 func (F *Form) Parse(r *http.Request) []string {
-	return []string{"a"}
+	params := make([]string,0,1)
+
+	r.ParseForm()
+	for _,v := range r.PostForm {
+		if len(v) < 1 { continue }
+		params = append(params, v[0])
+	}
+
+	return params
 }
