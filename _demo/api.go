e255462b24a1f11175b6bda0f605bbca01607f84
lee <isme@live.com>
2022-01-28 12:13:28 +0800

    简化

--- _demo/api.go
+++ _demo/api.go
@@ -1,11 +1,8 @@
 package main
 
 import "github.com/boyxp/nova"
-import "github.com/boyxp/nova/request"
-import "github.com/boyxp/nova/response"
 import _ "api/controller"
 
 func main() {
-	nova.Listen(":8080").SetRequest(&request.Form{}).SetResponse(&response.Json{}).Run()
+	nova.Listen(":8080").Run()
 }
-
