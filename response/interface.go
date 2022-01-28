e8b390ab23a0d5391e2aedeb7b55f5215f7cb971
lee <isme@live.com>
2022-01-28 11:54:55 +0800

    json实现

--- response/interface.go
+++ response/interface.go
@@ -3,5 +3,6 @@ package response
 import "net/http"
 
 type Interface interface {
-	Render(w http.ResponseWriter)
+	Render(w http.ResponseWriter,result interface{})
+	Error(w http.ResponseWriter,message string, code int64)
 }
--- response/json.go
+++ response/json.go
@@ -1,9 +1,24 @@
 package response
 
 import "net/http"
+import "encoding/json"
+import "fmt"
+import "github.com/boyxp/nova/exception"
 
-type Json struct {}
+type Json struct {
+	w http.ResponseWriter
+}
 
-func (J *Json) Render(w http.ResponseWriter) {
+func (J *Json) Render(w http.ResponseWriter, result interface{}) {
 
+}
+
+func (J *Json) Error(w http.ResponseWriter,message string, code int64) {
+	res := &exception.Exception{message,code}
+	json, err := json.Marshal(res)
+	if err != nil {
+        fmt.Fprintf(w, "{\"message\":\"%v\",\"code\":0}", err)
+    } else {
+		fmt.Fprintf(w, string(json))
+	}
 }
