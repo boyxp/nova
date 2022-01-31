d0bb8e5abc29ad2c0f6e313e65894a92d0d793a5
lee <isme@live.com>
2022-01-31 19:43:46 +0800

    返回值和空返回值支持

--- _demo/controller/User.go
+++ _demo/controller/User.go
@@ -9,24 +9,22 @@ func init() {
 }
 
 type User struct {}
-func (C *User) Login(name string, age uint64, check bool, balance float64, num int64) string {
-	fmt.Println(age)
+func (C *User) Login(name string, age uint64, check bool, balance float64, num int64) interface{} {
+	fmt.Println("姓名：",name,"年龄：",age,"检查：",check,"余额：",balance,"数量：",num)
 
 	if age<18 {
 		exception.New("年龄最小18岁", 101)
 	}
 
 	if age > 100 {
-		panic("年龄太大")
+		panic("年龄参数异常")
 	}
 
-	fmt.Println(name, age, check, balance, num)
-
-	return name;
+	return map[string]interface{}{"Name":name}
 }
 
-func (C *User) Logout() string {
-	return "bye"
+func (C *User) Logout() {
+	fmt.Println("bye")
 }
 
 func (C *User) private() {
