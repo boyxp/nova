dc420c6a4543b739d7d6bee0e06434954d5825c9
lee <isme@live.com>
2022-02-01 08:52:44 +0800

    打印时间

--- _demo/controller/User.go
+++ _demo/controller/User.go
@@ -1,6 +1,7 @@
 package controller
 
 import "fmt"
+import "time"
 import "github.com/boyxp/nova/router"
 import "github.com/boyxp/nova/exception"
 
@@ -10,7 +11,7 @@ func init() {
 
 type User struct {}
 func (C *User) Login(name string, age uint64, check bool, balance float64, num int64) interface{} {
-	fmt.Println("姓名：",name,"年龄：",age,"检查：",check,"余额：",balance,"数量：",num)
+	fmt.Println(time.Now().Format("2006-01-02 15:03:04"),"姓名：",name,"年龄：",age,"检查：",check,"余额：",balance,"数量：",num)
 
 	if age<18 {
 		exception.New("年龄最小18岁", 101)
