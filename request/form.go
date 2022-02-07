f920fd2a028a48d68bf5d8edc4fbe07cde703be5
lee <isme@live.com>
2022-02-07 19:57:08 +0800

    兼容GET和POST

--- request/form.go
+++ request/form.go
@@ -1,27 +1,18 @@
 package request
 
 import "net/http"
-import "io/ioutil"
-import "strings"
 
 type Form struct {}
 
 func (F *Form) Parse(r *http.Request)map[string]string {
 	params := map[string]string{}
 
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return params
-	}
-
-	pairs := strings.Split(string(body), "&")
-
-	for i:=0;i<len(pairs);i++ {
-		pos := strings.Index(pairs[i], "=")
-		key := pairs[i][0:pos]
-		val := pairs[i][pos+1:]
-		params[key] = val
-	}
+	r.ParseForm()
+   	if len(r.Form) > 0 {
+      for k,v := range r.Form {
+         params[k] = v[0]
+      }
+    }
 
 	return params
 }
