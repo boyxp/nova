c54cd07cd72bd40ebd926c800c34bdf9985176b8
lee <isme@live.com>
2022-02-15 10:11:41 +0800

    支持文件上传

--- request/form.go
+++ request/form.go
@@ -1,18 +1,56 @@
 package request
 
+import "io"
+import "os"
+import "time"
+import "strings"
+import "strconv"
 import "net/http"
+import "io/ioutil"
 
 type Form struct {}
 
 func (F *Form) Parse(r *http.Request)map[string]string {
 	params := map[string]string{}
 
-	r.ParseForm()
+   contentType := r.Header.Get("Content-Type")
+
+   if(contentType=="application/x-www-form-urlencoded") {
+	   r.ParseForm()
    	if len(r.Form) > 0 {
-      for k,v := range r.Form {
-         params[k] = v[0]
+         for k,v := range r.Form {
+            params[k] = v[0]
+         }
       }
-    }
+   }
+
+   if(strings.Contains(contentType, "multipart/form-data")) {
+      reader, err := r.MultipartReader()
+      if err == nil {
+         for {
+            part, err := reader.NextPart()
+            if err == io.EOF {
+               break
+            }
+
+            if part.FileName() == "" {
+                  data, _  := ioutil.ReadAll(part)
+                  k        := part.FormName()
+                  v        := string(data)
+                  params[k] = v
+
+            } else {
+               k      := part.FormName()
+               v      := "./"+strconv.FormatInt(time.Now().UnixNano(), 10)+"_"+part.FileName()
+               dst, _ := os.Create(v)
+               defer dst.Close()
+               io.Copy(dst, part)
+
+               params[k] =v
+            }
+         }
+      }
+   }
 
 	return params
 }
