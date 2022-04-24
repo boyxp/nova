2d31a9513483cb6d3f44e16195ace08b23d0ad97
lee <isme@live.com>
2022-04-24 09:53:20 +0800

    示例

--- _demo/api.go
+++ _demo/api.go
@@ -1,8 +1,10 @@
 package main
 
 import "github.com/boyxp/nova"
+import "github.com/boyxp/nova/database"
 import _ "api/controller"
 
 func main() {
+	database.Register("database", "dev_xinzhanghu", "root:123456@tcp(localhost:3306)/dev_xinzhanghu")
 	nova.Listen("8080").Run()
 }
