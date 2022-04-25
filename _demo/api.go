1a7b1819a7453708d864c68c8c6586af30be5a85
lee <isme@live.com>
2022-04-25 10:18:15 +0800

    数据库

--- _demo/api.go
+++ _demo/api.go
@@ -5,6 +5,6 @@ import "github.com/boyxp/nova/database"
 import _ "api/controller"
 
 func main() {
-	database.Register("database", "dev_xinzhanghu", "root:123456@tcp(localhost:3306)/dev_xinzhanghu")
+	database.Register("database", "test", "root:123456@tcp(localhost:3306)/test")
 	nova.Listen("8080").Run()
 }
