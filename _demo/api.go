76b8bb64dde7521cfc4bd57cfc061a2c2b14e728
lee <isme@live.com>
2022-04-27 11:45:58 +0800

    改为dbname

--- _demo/api.go
+++ _demo/api.go
@@ -6,6 +6,6 @@ import "github.com/boyxp/nova/database"
 import _ "api/controller"
 
 func main() {
-	database.Register("database", os.Getenv("database.db"), os.Getenv("database.dsn"))
+	database.Register("database", os.Getenv("database.dbname"), os.Getenv("database.dsn"))
 	nova.Listen(os.Getenv("port")).Run()
 }
