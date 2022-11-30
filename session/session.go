cee2b740ba11fca51239ab45b49227ee68fd305c
lee <isme@live.com>
2022-11-30 15:23:26 +0800

    封装

--- session/session.go
+++ session/session.go
@@ -1,46 +1,15 @@
 package session
 
 import "os"
-import "sync"
 import "strconv"
 import "strings"
 import "io/ioutil"
-import "github.com/techoner/gophp"
+import "github.com/leeqvip/gophp"
 import "github.com/boyxp/nova/cookie"
 
-var cache sync.Map
-var path string = "/var/lib/php/session/"
-
-func Config(set string) bool {
-	path = set
-	return true
-}
-
 func Get(name string) string {
-	ssid := cookie.Get("PHPSESSID")
-	if ssid == "" {
-		return ""
-	}
-
-	file, err := os.Open(path+"/sess_"+ssid)
-	defer file.Close()
-   	if err != nil {
-        return ""
-   	}
-
-   content, err := ioutil.ReadAll(file)
-   str := string(content)
-   if len(str)==0 {
-   	    return ""
-   }
-
-   if strings.Contains(str, "|") {
-   		str = str[strings.Index(str, "|")+1:]
-   }
-
-	data, _  := gophp.Unserialize([]byte(str))
-	sess, ok := data.(map[string]interface{})
-	if !ok {
+	sess := read()
+	if sess==nil {
 		return ""
 	}
 
@@ -70,4 +39,54 @@ func Get(name string) string {
 
 func Set(name string, value string) bool {
 	return true
+}
+
+func ssid() string {
+	ssid := cookie.Get("PHPSESSID")
+	if ssid != "" {
+		return ssid
+	}
+
+	ssid = cookie.Get("GOSESSID")
+	return ssid
+}
+
+func path() string {
+	_path := os.Getenv("session.path")
+	if _path != "" {
+		return _path
+	}
+
+	return "/var/lib/php/session/"
+}
+
+func read() map[string]interface{} {
+	var res map[string]interface{}
+
+	ssid := ssid()
+	path := path()
+
+	file, err := os.Open(path+"/sess_"+ssid)
+	defer file.Close()
+   	if err != nil {
+        return res
+   	}
+
+   content, err := ioutil.ReadAll(file)
+   str := string(content)
+   if len(str)==0 {
+   	    return res
+   }
+
+   if strings.Contains(str, "|") {
+   		str = str[strings.Index(str, "|")+1:]
+   }
+
+	data, _  := gophp.Unserialize([]byte(str))
+	sess, ok := data.(map[string]interface{})
+	if !ok {
+		return res
+	}
+
+	return sess
 }
