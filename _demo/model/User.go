3d21ce88f41fe98eff85ccc2378a1e5108e9ebc4
lee <isme@live.com>
2023-06-12 16:23:21 +0800

    模型继承示例

--- _demo/model/User.go
+++ _demo/model/User.go
@@ -2,4 +2,18 @@ package model
 
 import "github.com/boyxp/nova/database"
 
+//简单模型，直接操作表
 var User = database.Model{"user"}
+
+/*
+//复杂模型，继承附加方法
+type ModelUser struct {
+	database.Model
+}
+
+func (M *ModelUser) MaxUserId() string {
+	return M.Field("max(user_id) as max_id").Value("max_id")
+}
+
+var User = ModelUser{database.Model{"user"}}
+*/
