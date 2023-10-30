41a6d9b7cd1a7596dbc75f2d12174411e6ae62e1
lee <isme@live.com>
2023-10-30 10:19:21 +0800

    新增throw方法

--- exception/exception.go
+++ exception/exception.go
@@ -4,6 +4,10 @@ func New(message string, code int64) {
 	panic(&Exception{message, code})
 }
 
+func Throw(message string, code int64) {
+	panic(&Exception{message, code})
+}
+
 type Exception struct {
 	Message string `json:"message"`
 	Code int64 `json:"code"`
