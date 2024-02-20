963a998931adf2d8bb57e67735e6b9ff4775c3eb
lee <isme@live.com>
2024-02-20 09:40:04 +0800

    改为demo表

--- _demo/model/User.go
+++ _demo/model/User.go
@@ -3,7 +3,7 @@ package model
 import "github.com/boyxp/nova/database"
 
 //简单模型，直接操作表
-var User = database.Model{"user"}
+var User = database.Model{"demo"}
 
 /*
 //复杂模型，继承附加方法
@@ -15,5 +15,5 @@ func (M *ModelUser) MaxUserId() string {
 	return M.Field("max(user_id) as max_id").Value("max_id")
 }
 
-var User = ModelUser{database.Model{"user"}}
+var User = ModelUser{database.Model{"demo"}}
 */
