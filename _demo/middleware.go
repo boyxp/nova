6432bf9530809c62f0a2ff65806b898ebe20255d
lee <isme@live.com>
2024-06-15 18:09:14 +0800

    简化

--- _demo/middleware.go
+++ _demo/middleware.go
@@ -1,15 +1,12 @@
 package main
 
-import "os"
 import "log"
 import "net/http"
 import "github.com/boyxp/nova"
-import "github.com/boyxp/nova/database"
 import _ "api/controller"
 
 func main() {
-	database.Register("database", os.Getenv("database.dbname"), os.Getenv("database.dsn"))
-	nova.Listen(os.Getenv("port")).Use(logger).Run()
+	nova.Listen().Use(logger).Run()
 }
 
 func logger(next http.Handler) http.Handler {
